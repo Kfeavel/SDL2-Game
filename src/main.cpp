f57925c6e83388a7b0e372195361e0a359e9c930
Keeton Feavel <keeton@xyr.is>
2022-04-10 18:18:34 -0400

    Display `.bmp` file with SDL2

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,89 @@
-#include "SDL.h"
+#include "Logger/Logger.hpp"
+#include <filesystem>
+#include <cstdlib>
+#include <SDL.h>
+
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 800
+
+static SDL_Window* gWindow = nullptr;
+static SDL_Surface* gScreenSurface = nullptr;
+static SDL_Surface* gHelloWorld = nullptr;
+
+static bool Initialize()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        Logger::Fatal() << SDL_GetError() << std::endl;
+        return false;
+    } else {
+        gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (gWindow == nullptr) {
+            Logger::Fatal() << SDL_GetError() << std::endl;
+            return false;
+        }
+
+        gScreenSurface = SDL_GetWindowSurface(gWindow);
+    }
+
+    Logger::Info() << "Initialized SDL" << std::endl;
+    return true;
+}
+
+static bool LoadAssets()
+{
+    gHelloWorld = SDL_LoadBMP("assets/hello_world.bmp");
+    if (gHelloWorld == nullptr) {
+        Logger::Fatal() << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    Logger::Info() << "Loaded assets" << std::endl;
+    return true;
+}
+
+static void Destroy()
+{
+    Logger::Info() << "Destroying engine..." << std::endl;
+    SDL_FreeSurface(gHelloWorld);
+    gHelloWorld = nullptr;
+    SDL_DestroyWindow(gWindow);
+    gWindow = nullptr;
+
+    SDL_Quit();
+}
 
 int main(int argc, char* argv[])
 {
-    SDL_Init(SDL_INIT_VIDEO);
+    (void)argc;
+    (void)argv;
 
-    SDL_Window* window = SDL_CreateWindow("Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+    Logger::Info() << "cwd: " << std::filesystem::current_path() << std::endl;
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
+    bool quit = false;
+    if (Initialize()) {
+        if (LoadAssets()) {
+            SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+            SDL_UpdateWindowSurface(gWindow);
 
-    SDL_Delay(3000);
+            // TODO: Move event loop elsewhere.
+            // Event loop is required for macOS to display window
+            SDL_Event e;
+            while (!quit) {
+                while (SDL_PollEvent(&e)) {
+                    if (e.type == SDL_QUIT) {
+                        quit = true;
+                    }
+                }
+            }
+        } else {
+            Logger::Fatal() << "Failed to load all assets" << std::endl;
+            return EXIT_FAILURE;
+        }
+    } else {
+        Logger::Fatal() << "Failed to initialize" << std::endl;
+        return EXIT_FAILURE;
+    }
 
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
-    return 0;
+    Destroy();
+    return EXIT_SUCCESS;
 }
